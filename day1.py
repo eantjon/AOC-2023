a0d77d47c2c52db9d7ae346660bdf72af65b81d8
Ean Tjon <ean.tjon@hotmail.com>
2023-12-01 19:20:23 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,72 @@
+input_data=[
+'1abc2',
+'pqr3stu8vwx',
+'a1b2c3d4e5f',
+'treb7uchet'
+]
+
+def problem_1(input_data):
+    total_sum = 0
+    for word in input_data:
+        combi = ''
+        for i in word:
+            if i.isnumeric():
+                combi += i
+                break
+        for i in word[::-1]:
+            if i.isnumeric():
+                combi += i
+                break
+        total_sum += int(combi)
+    return(total_sum)
+
+def problem_2(input_data):
+    total_sum = 0
+    numbers = {"one": "1",
+               "two": "2",
+               "three": "3",
+               "four": "4",
+               'five': "5",
+               "six": "6",
+               "seven": "7",
+               "eight": "8",
+               "nine": "9"}
+
+    for word in input_data:
+        combi = ''
+        for i in range(len(word)):
+            if word[i].isnumeric():
+                combi += word[i]
+                break
+            found_word = False
+            for key,value in numbers.items():
+                try:
+                    if word[i:i+len(key)+1] == key:
+                        combi += value
+                        found_word = True
+                        break
+                except:
+                    pass
+            if found_word:
+                break
+            
+        for i in range(len(word)-1, -1, -1):
+            if word[i].isnumeric():
+                combi += word[i]
+                break
+            found_word= False
+            for key in numbers:
+                try:
+                    if word[i-len(key)+1:i+1] == key:
+                        combi += numbers[key]
+                        found_word = True
+                        break
+                except:
+                    pass
+            if found_word:
+                break
+        total_sum += int(combi)
+    return(total_sum)
+
+print(problem_1(input_data))
+print(problem_2(input_data))
